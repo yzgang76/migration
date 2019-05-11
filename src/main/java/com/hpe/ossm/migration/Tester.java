c8900e023863110755465a49bb67a65e364e0649
yanzhig <zhi-gang.yan@hpe.com>
2019-05-11 11:51:26 +0800

    update tool

--- src/main/java/com/hpe/ossm/migration/Tester.java
+++ src/main/java/com/hpe/ossm/migration/Tester.java
@@ -1,11 +1,20 @@
 package com.hpe.ossm.migration;
 
-import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
 
 public class Tester {
-    public static void main(String[] args){
-        final Config dc = ConfigFactory.load("selfmonitor");
+    public static void main(String[] args) {
+        ConfigFactory.load("selfmonitor");
+        ConfigFactory.load("wsrestservice");
+        ConfigFactory.load("cmglobal");
+        ConfigFactory.load("dcglobal");
+        ConfigFactory.load("selfmonitor_presenter");
+        ConfigFactory.load("sink");
+        ConfigFactory.load("receiverglobal");
+        ConfigFactory.load("receiverservice");
+        ConfigFactory.load("receiverconsole");
+        ConfigFactory.load("bulk_operation");
+        ConfigFactory.load("uoc2config");
     }
 }
